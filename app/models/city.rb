839ecd7b04c7c69a1d0ceac547cf659ffa38ef5d
--add <andrewksmoker@gmail.com>
2020-12-17 17:14:09 -0500

    commit tree 90e680d87d31425249e629f2e4b561b3b5b960d3

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,21 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, :through => :listings 
+
+  def city_openings(start_date, end_date)
+    openings(start_date, end_date)
+  end
+
+  def openings(start_date, end_date)
+    listings.merge(Listing.available(start_date, end_date))
+  end
+
+  def self.most_res
+    all.max do |a, b|
+      a.reservations.count <=> b.reservations.count 
+    end
+  end
 
 end
 
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,35 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+  private 
+
+  def city_openings(start_date, end_date)
+    openings(start_date, end_date)
+  end
+
+  def openings(start_date, end_date)
+    listings.merge(Listing.available(start_date, end_date))
+  end
+
+  def ratio_reservations_to_listings
+    if listings.count > 0
+      reservations.count.to_f / listings.count.to_f
+    end
+  end
+
+    # use of 'class_methods' is good, but I think is something that the curriculum 
+    # does not currently cover, so would need to be added.
+    def self.highest_ratio_reservations_to_listings
+
+      all.max do |a, b|
+        a.ratio_reservations_to_listings <=> b.ratio_reservations_to_listings
+      end
+    end
+
+  def self.most_res
+    all.max do |a, b|
+      a.reservations.count <=> b.reservations.count 
+    end
+  end
+
 end
